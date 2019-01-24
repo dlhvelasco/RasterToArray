47da4b4771df28fd5f93f827fe8616b905816891
Dwight Louis Velasco <dwight.velasco@datos-postprocessing.asti.local>
2019-01-24 14:10:27 +0800

    Subprocessed gdalbuildvrt -> rasterio array; also added gitignore

--- RasterToArray.py
+++ RasterToArray.py
@@ -3,18 +3,25 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from osgeo import gdal
+import subprocess
 
-from pygeotools.lib import iolib, warplib, geolib, timelib, malib
 
-
-filepath = "/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/HDFs/stacktest2"
+filepath = "/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/HDFs/stacktests/"
 
 # ds = gdal.Open(filepath)
 # myarray = np.array(ds.GetRasterBand(1).ReadAsArray())
 # print(myarray.shape)
 # print(myarray)
 
-with rasterio.open(filepath, 'r') as ds:
+subprocess.call(["gdalbuildvrt", "stacktest5.vrt", "-resolution", "highest", "-srcnodata", "-9999",                     #Saved in RasterToArray folder
+    filepath+"MYD04_3K.A2014001.mosaic.061.2018342082532.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf", 
+    filepath+"MYD04_3K.A2014002.mosaic.061.2018342082539.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
+    filepath+"MYD04_3K.A2014003.mosaic.061.2018342082539.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
+    filepath+"MYD04_3K.A2014004.mosaic.061.2018342082553.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf"
+    ])
+
+with rasterio.open("stacktest5.vrt", 'r') as ds: #Saved in RasterToArray folder
     arr = ds.read()  # read all raster values
 
 print(arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
+print(arr)
