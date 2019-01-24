613ed9d606c49d678a91e7ddb4d16d6282ccfec8
Dwight Louis Velasco <dwight.velasco@datos-postprocessing.asti.local>
2019-01-24 13:24:21 +0800

    Gdal or rasterio options

--- RasterToArray.py
+++ RasterToArray.py
@@ -1,11 +1,20 @@
 import rasterio
-import rasterio.plot
-import pyproj
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import pandas as pd
+from osgeo import gdal
 
-#test commit
-#wincred test
-#cache test
+from pygeotools.lib import iolib, warplib, geolib, timelib, malib
+
+
+filepath = "/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/HDFs/stacktest2"
+
+# ds = gdal.Open(filepath)
+# myarray = np.array(ds.GetRasterBand(1).ReadAsArray())
+# print(myarray.shape)
+# print(myarray)
+
+with rasterio.open(filepath, 'r') as ds:
+    arr = ds.read()  # read all raster values
+
+print(arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
