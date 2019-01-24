33627cd7b2f67a386c6e0673d8f6e61a8b89bde2
Dwight Louis Velasco <dwight.velasco@datos-postprocessing.asti.local>
2019-01-24 15:53:47 +0800

    Code for data cubes

--- RasterToArray.py
+++ RasterToArray.py
@@ -12,15 +12,27 @@ filepath = "/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/HDFs/st
 # print(myarray.shape)
 # print(myarray)
 
-subprocess.call(["gdalbuildvrt", "-resolution", "highest", "-separate", "-srcnodata", "-9999", "stacktest6",                   #Saved in RasterToArray folder
-    filepath+"MYD04_3K.A2014001.mosaic.061.2018342082532.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
-    filepath+"MYD04_3K.A2014002.mosaic.061.2018342082539.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
-    filepath+"MYD04_3K.A2014003.mosaic.061.2018342082539.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
-    filepath+"MYD04_3K.A2014004.mosaic.061.2018342082553.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf"
-    ])
+# subprocess.call(["gdalbuildvrt", "-resolution", "highest", "-separate", "-srcnodata", "-9999", "stacktest6", #Saved in RasterToArray folder
+#     filepath+"MYD04_3K.A2014001.mosaic.061.2018342082532.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
+#     filepath+"MYD04_3K.A2014002.mosaic.061.2018342082539.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
+#     filepath+"MYD04_3K.A2014003.mosaic.061.2018342082539.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf",
+#     filepath+"MYD04_3K.A2014004.mosaic.061.2018342082553.psmcrpgscs_000501286999.Corrected_Optical_Depth_Land_2.hdf"
+#     ])
 
-with rasterio.open("stacktest6", 'r') as ds: #Saved in RasterToArray folder
+with rasterio.open("stacktest6", 'r') as ds:  # Saved in RasterToArray folder
     arr = ds.read()  # read all raster values
 
-print(arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
-print(arr)
+    # Same as arr=ds.read()
+    bandlist = list(ds.indexes)
+    array = np.zeros((len(bandlist), 611, 360))
+    array[:, :, :] = ds.read(bandlist)
+
+print("Array shape:", arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
+print("Source indices:", ds.indexes)
+print("Cell centroid:", ds.xy(0, 0))  # Displays centroid of cell 0,0
+print("Source bounds:", ds.bounds)
+row, col = ds.index(ds.bounds.right, ds.bounds.bottom)
+print(row, col)
+
+print(arr[1, row-1, col-1])
+print(array[1, row-1, col-1])
