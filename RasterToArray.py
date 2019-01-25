00f9dde158ed12d8b7069e83af2e6ca78d6311be
Dwight Louis Velasco <dwight.velasco@datos-postprocessing.asti.local>
2019-01-25 16:34:06 +0800

    Work on obtaining row,col given coords

--- RasterToArray.py
+++ RasterToArray.py
@@ -1,4 +1,4 @@
-import rasterio
+import rasterio 
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -6,13 +6,14 @@ from osgeo import gdal
 import subprocess
 
 filepath = "/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/datacube/"
+outfile = r"/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/datacube/test_{}.hdf"
 
 # ds = gdal.Open(filepath)
 # myarray = np.array(ds.GetRasterBand(1).ReadAsArray())
 # print(myarray.shape)
 # print(myarray)
 
-subprocess.call(["gdalbuildvrt", "-resolution", "highest", "-separate", "stacktest8", #Saved in RasterToArray folder
+subprocess.call(["gdalbuildvrt", "-resolution", "highest", "-separate", "stacktest8",  #Saved in RasterToArray folder
     filepath+"MYD04_3K.A2015001.mosaic.061.2019025050128.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
     filepath+"MYD04_3K.A2015002.mosaic.061.2019025050134.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
     filepath+"MYD04_3K.A2015003.mosaic.061.2019025050120.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
@@ -23,6 +24,13 @@ subprocess.call(["gdalbuildvrt", "-resolution", "highest", "-separate", "stackte
     ])
 
 row, col = 200, 160  # Arbitrary point in Luzon
+
+coordinates = (
+    (8.95581, 125.59708) , # lon, lat of CSU Ground, Brgy. Ampayon, Butuan City
+    (8.95499, 125.52679) , # lon, lat of Butuan City ENR Office Compound, Brgy. Doongan, Butuan City.
+)
+
+N = 1
 
 with rasterio.open("stacktest8", 'r') as ds:  # Saved in RasterToArray folder
     arr = ds.read()  # read all raster values
@@ -39,17 +47,35 @@ with rasterio.open("stacktest8", 'r') as ds:  # Saved in RasterToArray folder
 
     vals2 = [x for x in ds.sample([ds.xy(row-1, col-1, offset='ul')])]
 
-    # coords = ds.xy(row-1, col-1, offset='ul')
-    # vals = ds.sample(coords)
+    ######################################################################################################
+    # Obtain row,col given coordinates
+    print(ds.index(8.95, 125.59)) # Work on this
+    
+    for i, (lat, lon) in enumerate(coordinates):
+        px, py = ds.index(lat, lon)
+        print('Pixel X, Y coords: {}, {}'.format(px, py))
+        
+
+        window = rasterio.windows.Window(px, py, N, N)
+        print(window)
+
+        # Read the data in the window
+        # clip is a nbands * N * N numpy array
+        clip = ds.read(window=window)
+
+        # You can then write out a new file
+        meta = ds.meta
+        meta['width'], meta['height'] = N, N
+        meta['transform'] = rasterio.windows.transform(window, ds.transform)
+
+        with rasterio.open(outfile.format(i), 'w', **meta) as dst:
+            dst.write(clip)
 
 ######################################################################################################
-print("Array shape:", arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
+print("\nArray shape:", arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
 print("Source indices:", ds.indexes)
 print("Cell upper left:", ds.xy(0, 0, offset='ul'))  # Centroid OR offset to one of ul, ur, ll, lr
 print("Source bounds:", ds.bounds)
-
-print("# Rows:", row, "#Cols", col)
-print(ds.xy(row, col))
 
 aff = ds.transform
 print("Cell width:", aff[0], "Cell height:", -aff[4])
@@ -60,7 +86,9 @@ print("Cell width:", aff[0], "Cell height:", -aff[4])
 # 4 = height of a pixel (typically negative)
 # 5 = y-coordinate of the of the upper-left corner of the upper-left pixel
 ######################################################################################################
-print("Pixel value:", arr[0, row-1, col-1])  # Band 1 aka Jan 01
-print("Pixel value 2:", array[1, row-1, col-1])  # Band 2 aka Jan 02
+print("\nLocation of arbitrary point in Luzon:", ds.xy(row, col))
+
+print("Pixel value:", arr[1, row-1, col-1])  # Band 1 aka Jan 01
+print("Pixel value 2:", array[2, row-1, col-1])  # Band 2 aka Jan 02
 print(vals1)
-print(vals2)
+print(vals2,"\n")
