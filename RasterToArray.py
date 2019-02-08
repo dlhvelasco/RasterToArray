86585f5c1ea8b32a7c39eb8d113604a27381bf41
Dwight Louis Velasco <dwight.velasco@datos-postprocessing.asti.local>
2019-02-08 12:20:19 +0800

    Updated to new boundaries (NDRRMC based)

--- RasterToArray.py
+++ RasterToArray.py
@@ -1,72 +1,61 @@
 import rasterio
 import numpy as np
-# from osgeo import gdal
 import subprocess
 import shapely.speedups
 
 shapely.speedups.enable()
 
-filepath = "/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/datacube/"
+filepath = r"/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/datacube/"
 outfile = r"/home/dwight.velasco/dwight.velasco/scratch1/THESIS/MYD04_3K/datacube/test_{}.hdf"
 
-# ds = gdal.Open(filepath)
-# myarray = np.array(ds.GetRasterBand(1).ReadAsArray())
-# print(myarray.shape)
-# print(myarray)
-
-# subprocess.call(["gdalbuildvrt", "-resolution", "highest", "-separate", "stacktest8",  #Saved in RasterToArray folder
-#     filepath+"MYD04_3K.A2015001.mosaic.061.2019025050128.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
-#     filepath+"MYD04_3K.A2015002.mosaic.061.2019025050134.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
-#     filepath+"MYD04_3K.A2015003.mosaic.061.2019025050120.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
-#     filepath+"MYD04_3K.A2015004.mosaic.061.2019025050135.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
-#     filepath+"MYD04_3K.A2015005.mosaic.061.2019025050121.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
-#     filepath+"MYD04_3K.A2015006.mosaic.061.2019025050115.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf",
-#     filepath+"MYD04_3K.A2015007.mosaic.061.2019025050123.psmcrpgscs_000501298443.Optical_Depth_Land_And_Ocean.hdf"
-#     ])
+###########################################################################
+subprocess.call(["gdalbuildvrt", "-resolution", "highest", "-separate",
+                "-input_file_list", "MYD043kfileList.txt", "stacktest8.vrt",
+                 ])
+###########################################################################
 
 # row, col = 199, 159  # Arbitrary point in Luzon
-
-# Set scale factor for ERA5
+# Set scale factor, offset for ERA5 data
 scale_factor = 0.0007222299746216393
 offset = 0.5313933787260681
 
 coordinates = [
     # (21.12, 114.27)  # upper left (0,0)
     # (8.95581, 125.59708),  # lat, lon of CSU Ground, Brgy. Ampayon, Butuan City
-    # (8.95499, 125.52679),  # lat, lon of Butuan City ENR Office Compound, Brgy. Doongan, Butuan City.
+    (8.95499, 125.52679),  # lat, lon of Butuan City ENR Office Compound, Brgy. Doongan, Butuan City.
     (15.686770491803276, 121.22516713091922)  # Arbitrary pt in Luzon
 ]
 
 # ERA5_2014_10m_u_component_of_wind.nc
-# stacktest8
-with rasterio.open("ERA5_2014_10m_u_component_of_wind.nc", 'r') as ds:  # Saved in RasterToArray folder
+# stacktest8.vrt
+# MYD043K.hdf
+with rasterio.open("stacktest8.vrt", 'r') as ds:  # Saved in RasterToArray folder
     arr = ds.read()  # read all raster values
 
     # Same as arr=ds.read()
     bandlist = list(ds.indexes)
     # array = np.zeros((len(bandlist), 619, 459))
-    array = np.zeros((len(bandlist), arr.shape[1], arr.shape[2]))  #[band, row, col]
+    array = np.zeros((len(bandlist), arr.shape[1], arr.shape[2]))  # [band, row, col]
     array[:, :, :] = ds.read(bandlist)
 
-    ######################################################################################################
+    ###########################################################################
     # vals1 = []
     # for val in ds.sample([ds.xy(row, col, offset='ul')]):
     #     vals1.append(val)
 
     # vals2 = [x for x in ds.sample([ds.xy(row, col, offset='ul')])]
 
-    ######################################################################################################
+    ###########################################################################
     # Print all bands at a point location
     # Doesnt work? See alt code below
     # for val in ds.sample(coordinates):
     #     print("VAL:", val)
 
-    ######################################################################################################
+    ###########################################################################
     # Obtain row,col given coordinates
-
     for i, (lat, lon) in enumerate(coordinates):
         px, py = ds.index(lon, lat)  # LON = x, LAT =  y
-        print('Pixel X, Y coords: {}, {}'.format(px, py))
+        print('\nPixel X, Y coords: {}, {}'.format(px, py))
 
         # Build an NxN window
         window = rasterio.windows.Window(py, px, 1, 1)  # py, px is ul corner; 1 x 1 window
@@ -93,7 +82,7 @@ with rasterio.open("ERA5_2014_10m_u_component_of_wind.nc", 'r') as ds:  # Saved 
         # with rasterio.open(outfile.format(i), 'w', **meta) as dst:
         #     dst.write(clip)
 
-######################################################################################################
+###############################################################################
 aff = ds.transform
 print("\nArray shape:", arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
 print("Number of dates:", len(ds.indexes))
@@ -109,7 +98,7 @@ print("Cell width:", aff[0], "Cell height:", -aff[4])
 # 4 = height of a pixel (typically negative)
 # 5 = y-coordinate of the of the upper-left corner of the upper-left pixel
 
-######################################################################################################
+###############################################################################
 # print("\nLocation of arbitrary point in Luzon:", ds.xy(row, col))
 
 # print("Pixel value:", arr[1, row, col])  # Band 1 aka Jan 01
--- pointspergrid.py
+++ pointspergrid.py
@@ -2,9 +2,12 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
 import geopandas.tools
+import shapely.speedups
+
+shapely.speedups.enable()
 
 # Read the data.
-polygons = gpd.GeoDataFrame.from_file('/home/dwight.velasco/dwight.velasco/scratch1/THESIS/RasterToArray/modisgrid/modisgrid.shp')
+polygons = gpd.GeoDataFrame.from_file('/home/dwight.velasco/dwight.velasco/scratch1/THESIS/RasterToArray/modisgrid/clipped.shp')
 
 # Point data is from VIIRS 02 Jan 2015 to 03 Jan 2015, containing 212 entries
 df = pd.read_csv(r'/home/dwight.velasco/dwight.velasco/scratch1/THESIS/FIRMS/DL_FIRE_V1_39825/fire_archive_V1_39825.csv')
@@ -22,7 +25,7 @@ sjoin = gpd.tools.sjoin(points, polygons, how='left')
 
 # converting geodataframe to dataframe
 df_sjoin = pd.DataFrame(sjoin)
-# add obs column set to 1 for every hit
+# add column set to 1 for every hit
 df_sjoin['fire_spots'] = 1
 
 cumulative_firespots = 0
@@ -37,10 +40,10 @@ for date, group in grouped:
     # map index_right values to coordinates of centroid inside cell
     # referencing this coordinate in RasterToArray.py later
     df2['coords'] = list(zip(df2['index_right'].map(df_sjoin.drop_duplicates('index_right').set_index('index_right')['longitude']),
-                             df2['index_right'].map(df_sjoin.drop_duplicates('index_right').set_index('index_right')['latitude'])))    
+                             df2['index_right'].map(df_sjoin.drop_duplicates('index_right').set_index('index_right')['latitude'])))
     print(df2.head())
-    print(len(df2.index)) # len returns number of grids with at least 1 hit for a given day
-    print(df2.fire_spots.sum()) # Sum of total fire spots for a given day
+    print(len(df2.index))  # len returns number of grids with at least 1 hit for a given day
+    print(df2.fire_spots.sum())  # Sum of total fire spots for a given day
     cumulative_firespots += df2.fire_spots.sum()
 
 ##################################################################
